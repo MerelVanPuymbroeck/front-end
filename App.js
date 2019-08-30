89a540785cb16a3f451f3613476a6c9df2127204
Merel van puymbroeck <merelvanpuymbroeck@gmail.com>
2019-08-30 09:34:25 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -2,7 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
+ constructor(props) {
+   super(props);
+
+   this.state = {
+     Title: null,
+     Author: null,
+     Message:null,
+     formErrors: {
+       Title: "",
+       Author:"",
+       Message:"",
+     }
+   };
+ } 
+
+ handleSubmit = e => {
+   e.preventDefault();
+   {/* not going to another page but just staying on the mainpage*/}
+ }; 
+
+ render() {
     return (
       <div className="wrapper">
          <div className="form-wrapper">
@@ -22,6 +42,15 @@ class App extends Component {
               <label htmlFor="Message">Message</label>
               <input type="text" noValidate className="" placeholder="Message in here" type="text" name="Message" onChange={this.handleChange}/> 
               {/* not be validated when submitted*/}
+              </div>
+              <div className ="Submit">
+                <button type="submit">Submit</button>
+              </div>
+              <div className ="Submit">
+                <button type="submit">Edit</button>
+              </div>
+              <div className ="Submit">
+                <button type="submit">Delete</button>
               </div>
            </form>
         </div>
